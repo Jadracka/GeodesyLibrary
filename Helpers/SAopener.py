679b8ab80feb0fbba7464c6acafafb860cd758d9
Jan Hamal Dvořák <mordae@anilinux.org>
2024-01-30 13:31:01 +0100

    SA: clean up the helper module
    Signed-off-by: Jan Hamal Dvořák <mordae@anilinux.org>

--- Helpers/SAopener.py
+++ Helpers/SAopener.py
@@ -1,73 +1,91 @@
 import os
 import subprocess
 import time
-import psutil
 import winreg
+
+import psutil
 import wmi
 
-def dongle_check():
-#    VID = 0x04B9
-#    PID = 0x0300
+# License Key Dongle USB Vendor & Product IDs
+DONGLE_VID = 0x04B9
+DONGLE_PID = 0x0300
+
+
+class SAException(Exception):
+    """Errors related to our Spatial Analyzer integration"""
+
+
+def _is_dongle_present():
     wmi_inst = wmi.WMI()
     devices = wmi_inst.Win32_PnPDevice()
     device_ids = [dev.SystemElement.DeviceID for dev in devices]
+
     for devid in device_ids:
-        if ("VID_" + "04B9" + "&PID_" + "0300") in devid:
+        if f"VID_{DONGLE_VID:04X}&PID_{DONGLE_PID:04X}" in devid:
             return True
 
-def get_SApath_registry():
+
+def _get_SApath_registry():
     try:
         # Open the registry key
-        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 
-                             r"SOFTWARE\New River Kinematics\Spatial Analyzer\Log")
+        key = winreg.OpenKey(
+            winreg.HKEY_CURRENT_USER,
+            r"SOFTWARE\New River Kinematics\Spatial Analyzer\Log",
+        )
 
         # Read the registry value
         value, _ = winreg.QueryValueEx(key, r"EXE Path")
 
         # Close the registry key
         winreg.CloseKey(key)
-        
-        program_name = os.path.basename(value)
-        return (value, program_name)
-    except Exception as e:
-        print("It seems the SA is not installed on this computer.")
-        print(f"Error: {e}")
-        del e, key, value, program_name
+
+        return value
+
+    except Exception:
         return None
 
 
-def start_program(program_path, timeout=6):
+def _start_program(program_path, timeout=6):
     try:
-        subprocess.Popen(f'"{program_path}"', shell=True)
+        subprocess.Popen(
+            [program_path],
+            stdin=subprocess.DEVNULL,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+    except Exception:
+        return False
 
-        time.sleep(timeout)  # Wait for the program to start (you might need to adjust the delay)
+    # Wait for the program to start (you might need to adjust the delay)
+    time.sleep(timeout)
 
-        program_name = os.path.basename(program_path)
-        for proc in psutil.process_iter(attrs=['name']):
-            if proc.info['name'] == program_name:
-                return True  # Program started successfully
+    program_name = os.path.basename(program_path)
+    return _is_program_running(program_name)
 
-    except (subprocess.CalledProcessError, Exception):
-        pass
 
-    return False  # Program did not start or an error occurred
-
-def is_program_running(program_name):
-    for proc in psutil.process_iter(attrs=['name']):
-        if proc.info['name'] == program_name:
+def _is_program_running(program_name):
+    for proc in psutil.process_iter(attrs=["name"]):
+        if proc.name == program_name:
             return True
+
     return False
 
-def start_SA():
-    if not dongle_check():
-        print("SA Hardware key has not been found, is it plugged in?")
-    else:
-        path, name = get_SApath_registry()
-        if not is_program_running(name):
-            start_program(path)
-        if is_program_running(name):
-            print("SA has been succesfully opened.")
-    del path, name
-    return True
 
-start_SA()
+def start_SA():
+    if not _is_dongle_present():
+        raise SAException("SA Hardware key has not been found, is it plugged in?")
+
+    path = _get_SApath_registry()
+
+    if path is None:
+        raise SAException("It seems the SA is not installed on this computer.")
+
+    program_name = os.path.basename(path)
+
+    if not _is_program_running(program_name):
+        if not _start_program(path):
+            raise SAException("SA did not start or an error occurred")
+
+
+if __name__ == "__main__":
+    start_SA()
